ef71d0eebc13e67a3197ab1ad2ee2177efb7e3e9
Swelihle Lucas <Swelihle.Lucas@Kzntransport.gov.za>
2026-02-09 08:49:31 +0200

    refactor: Improve class order and structure in UI components for better readability

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       type={type}
       data-slot="input"
       className={cn(
-        'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground bg-background dark:bg-input border-input h-10 min-h-10 w-full min-w-0 rounded-lg border px-3 py-2 text-base shadow-sm transition-[color,box-shadow,border-color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 disabled:bg-muted md:text-sm',
+        'file:inline-flex bg-background selection:bg-primary disabled:bg-muted dark:bg-input file:bg-transparent disabled:opacity-50 shadow-sm px-3 py-2 border border-input file:border-0 rounded-lg outline-none w-full min-w-0 h-10 file:h-7 min-h-10 file:font-medium selection:text-primary-foreground placeholder:text-muted-foreground file:text-foreground md:text-sm file:text-sm text-base transition-[color,box-shadow,border-color] disabled:cursor-not-allowed disabled:pointer-events-none',
         'hover:border-ring/50',
         'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -14,7 +14,7 @@ function ResizablePanelGroup({
     <Group
       data-slot="resizable-panel-group"
       className={cn(
-        'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
+        'flex data-[panel-group-direction=vertical]:flex-col w-full h-full',
         className,
       )}
       {...props}
@@ -39,13 +39,13 @@ function ResizableHandle({
     <Separator
       data-slot="resizable-handle"
       className={cn(
-        'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
+        'after:left-1/2 data-[panel-group-direction=vertical]:after:left-0 after:absolute relative after:inset-y-0 flex justify-center items-center bg-border focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 w-px after:w-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:h-px [&[data-panel-group-direction=vertical]>div]:rotate-90 after:-translate-x-1/2 data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0',
         className,
       )}
       {...props}
     >
       {withHandle && (
-        <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
+        <div className="z-10 flex justify-center items-center bg-border border rounded-xs w-3 h-4">
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
